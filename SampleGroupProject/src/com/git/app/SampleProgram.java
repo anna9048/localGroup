1f4691783ab1cd5467e855560629ac8668baeef4
annakurian96 <annakurian96@gmail.com>
2020-02-26 10:16:46 +0530

    first commit

---
+++ SampleGroupProject/src/com/git/app/SampleProgram.java
@@ -0,0 +1,14 @@
+package com.git.app;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 //Looping
+		for(int i=0;i<10;i++){
+			System.out.println("Number : "+i);
+		}
+	}
+
+}

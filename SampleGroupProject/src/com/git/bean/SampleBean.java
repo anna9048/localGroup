cf15fb745c3b3c8bef2eac6d1c3de8cb1959a434
abinandav1995 <abinandav1995@gmail.com>
2020-02-26 11:16:44 +0530

    First commit

---
+++ SampleGroupProject/src/com/git/bean/SampleBean.java
@@ -0,0 +1,44 @@
+package com.git.bean;
+
+public class SampleBean {
+
+	private String name;
+	private int number;
+	
+	
+	public SampleBean(String name, int number) {
+		super();
+		this.name = name;
+		this.number = number;
+		
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getNumber() {
+		return number;
+	}
+
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+
+	public String toString() {
+		return "SampleBean [name=" + name + ", number=" + number + "]";
+	}
+	
+	
+	
+}
